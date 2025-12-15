83cc0c49e775481a1bf30cb3d9a9bc8e4966ae73
KingMalower <maciejzych2003@gmail.com>
2025-12-15 18:34:15 +0100

    Wyświetalnie gier + połowa dodawania, trzeba backend zrobić, po nazwa dodajemy nie po Id

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -2,10 +2,44 @@ import * as auth from '$lib/server/auth';
 import { fail, redirect } from '@sveltejs/kit';
 import { getRequestEvent } from '$app/server';
 import type { Actions, PageServerLoad } from './$types';
+import { drizzle } from 'drizzle-orm/mysql2';
+import { db } from '$lib/server/db';
+import { eq } from 'drizzle-orm';
+import { gra, miejsca, turniej, user } from '$lib/server/db/schema';
+import { alias } from 'drizzle-orm/mysql-core';
+
+const gracz = user;
 
 export const load: PageServerLoad = async () => {
 	const user = requireLogin();
-	return { user };
+
+	const gracz1 = alias(gracz, "gracz1");
+    const gracz2 = alias(gracz, "gracz2");
+	const gracz3 = alias(gracz, "gracz3");
+
+	const zwyciezca = alias(gracz, "zwyciezca");
+
+	return { user, gra: await db.select({
+		gracz1: gracz1.nazwa,
+		gracz2: gracz2.nazwa,
+		gracz3: gracz3.nazwa,
+
+		zwyciezca: zwyciezca.nazwa,
+
+		miejsce: miejsca.nazwa,
+
+		turniej: turniej.nazwa,
+
+		data: gra.data,
+		isRanked: gra.isRanked
+	}).from(gra)
+	.leftJoin(gracz1, eq(gra.graczID1, gracz1.id))
+	.leftJoin(gracz2, eq(gra.graczID2, gracz2.id))
+	.leftJoin(gracz3, eq(gra.graczID3, gracz3.id))
+	.leftJoin(zwyciezca, eq(gra.zwyciezca, zwyciezca.id))
+	.leftJoin(miejsca, eq(gra.miejsceID, miejsca.miejscaID))
+	.leftJoin(turniej, eq(gra.turniejID, turniej.turniejID))
+};
 };
 
 export const actions: Actions = {
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,7 +6,22 @@
 </script>
 
 <h1>Hi, {data.user.username}!</h1>
-<p>Your user ID is {data.user.id}.</p>
 <form method="post" action="?/logout" use:enhance>
 	<button>Sign out</button>
+</form>
+<h1>Dodaj nową grę</h1>
+<form method="post" action="?/addGame" use:enhance>
+<label for=""></label>
+<input type="text" name="gracz1" id="gracz1">
+<label for=""></label>
+<input type="text" name="gracz2" id="gracz2">
+<label for=""></label>
+<input type="text" name="gracz3" id="gracz3">
+<label for=""></label>
+<input type="text" name="zwycięzca" id="zwycięzca">
+<label for=""></label>
+<input type="checkbox" name="isRanked" id="isRanked">
+<input type="text" name="miejsceNazwa" id="miejsce">
+<label for=""></label>
+<input type="text" name="TurniejNazwa" id="turniej">
 </form>
