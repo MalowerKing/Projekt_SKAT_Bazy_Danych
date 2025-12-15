58db2e3d1b2568b81f9dfbee4eb0251c06336535
KingMalower <maciejzych2003@gmail.com>
2025-12-15 14:08:24 +0100

    Dodawanie miast i wyświetalnie ich

---
+++ src/routes/Pages/Miejsca/+page.server.ts
@@ -0,0 +1,55 @@
+import { fail } from '@sveltejs/kit';
+import { db } from '$lib/server/db'; 
+import { miejsca } from '$lib/server/db/schema';
+import { encodeBase32LowerCase } from '@oslojs/encoding';
+import type { PageServerLoad } from '../../$types';
+
+export const actions = {
+  // The 'default' action handles the form submission
+  addMiejsce: async ({ request }) => {
+    // 1. Get Form Data
+    const formData = await request.formData();
+    const nazwa = formData.get('nazwa');
+    const adres = formData.get('adres');
+    const miasto = formData.get('miasto');
+
+    const miejsceId = generateUserId();
+
+    // 2. Simple Validation
+    if (!nazwa || !adres || !miasto) {
+      // Return data back to form so user doesn't lose input
+      return fail(400, { 
+        nazwa, 
+        adres, 
+        miasto,
+        missing: true,
+        message: 'Wszystkie pola są wymagane'
+      });
+    }
+
+    // 3. (Optional) Database Insert Example
+    await db.insert(miejsca).values({ miejscaID: miejsceId.toString(), nazwa: nazwa.toString(), adres: adres.toString(), miasto: miasto.toString() });
+
+    console.log('Form received:', { miejsceId, nazwa, adres, miasto });
+
+    // 4. Return Success
+    return { success: true, message: 'Form submitted successfully!' };
+  }
+};
+
+export const load: PageServerLoad = async ({ params }) => {
+    return {
+            post: await db.select({
+                nazwa: miejsca.nazwa,
+                adres: miejsca.adres,
+                miasto: miejsca.miasto
+            }).from(miejsca)
+        };
+};
+
+function generateUserId() {
+    // ID with 120 bits of entropy, or about the same as UUID v4.
+    const bytes = crypto.getRandomValues(new Uint8Array(15));
+    const id = encodeBase32LowerCase(bytes);
+    return id;
+}
---
+++ src/routes/Pages/Miejsca/+page.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+  import { enhance } from '$app/forms';
+    import type { PageProps } from "./$types";
+
+  // 'form' comes from the return value of the action in +page.server.ts
+    
+
+    let {data, form}: PageProps = $props();
+  
+  let isLoading = false;
+</script>
+
+<form action="?/addMiejsce" method="post" use:enhance>
+    <label for="nazwa">Nazwa:</label>
+    <input type="text" name="nazwa" id="nazwa" value={form?.nazwa ?? ""}>
+    <label for="adres">Adres</label>
+    <input type="text" name="adres" id="adres" value={form?.adres ?? ""}>
+    <label for="miasto">Miasto</label>
+    <input type="text" name="miasto" id="miasto" value={form?.miasto ?? ""}>
+    <!-- svelte-ignore a11y_consider_explicit_label -->
+    <button formaction="?/addMiejsce">Zapisz</button>
+</form>
+
+{#each data.post as {nazwa, adres, miasto}}
+    <li>{nazwa} | {adres} | {miasto}</li>
+{/each}
