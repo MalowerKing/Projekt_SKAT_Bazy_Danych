4aa0b4ef5b72a71270a6db30f5f5869c15ebab9b
sobmi <michal.city2004@gmail.com>
2025-12-15 20:26:17 +0100

    Added Elo rank calculations.

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -4,9 +4,11 @@ import { getRequestEvent } from '$app/server';
 import type { Actions, PageServerLoad } from './$types';
 import { drizzle } from 'drizzle-orm/mysql2';
 import { db } from '$lib/server/db';
-import { eq } from 'drizzle-orm';
+import { eq, is } from 'drizzle-orm';
 import { gra, miejsca, turniej, user } from '$lib/server/db/schema';
 import { alias } from 'drizzle-orm/mysql-core';
+import { request } from 'http';
+import { fromAction } from 'svelte/attachments';
 
 const gracz = user;
 
@@ -51,6 +53,28 @@ export const actions: Actions = {
 		auth.deleteSessionTokenCookie(event);
 
 		return redirect(302, '/login');
+	},
+
+	addGame: async ({request}) => {
+		const formData = await request.formData();
+		const gracz1 = formData.get('gracz1') as string;
+		const gracz2 = formData.get('gracz2') as string;
+		const gracz3 = formData.get('gracz3') as string;
+		const zwyciezca = formData.get('zwyciezca') as string;
+		const miejsce = formData.get('miejsceNazwa') as string;
+		const turniej = formData.get('TurniejNazwa') as string;
+		const isRanked = formData.get('isRanked') === 'on';
+
+		if (!gracz1 || !gracz2 || !gracz3 || !zwyciezca || !miejsce || !turniej) {
+			return fail(400, {missing: true, message: 'Nie uzupełniono wszytskich danych formularza!'});
+		}
+
+		try {
+			await db.insert(gra).values({
+			});
+		} catch (error) {
+			
+		}
 	}
 };
 
@@ -62,4 +86,59 @@ function requireLogin() {
 	}
 
 	return locals.user;
+}
+
+
+// Elo ranking calculation logic
+function calculateEloProbability(rankFav: number, rankOpp: number): number {
+	return 1 / (1 + (10 ** ((rankOpp - rankFav) / 400)));
+}
+
+function calculateEloPoints(matchOutcome: number, winProbability: number, progressFactor: number = 40): number {
+	return (progressFactor * (matchOutcome - winProbability));
+}
+
+function calculateNewEloRank(player1Rank: number, player2Rank: number, player3Rank: number, winner: 1 | 2 | 3, progressFactor: number = 40): { player1NewRank: number; player2NewRank: number; player3NewRank: number } {
+	if (winner == 1) {
+		let vicWith2: number = calculateEloProbability(player1Rank, player2Rank);
+		let vicWith3: number = calculateEloProbability(player1Rank, player3Rank);
+
+		let eloPoints2: number = calculateEloPoints(1, vicWith2, progressFactor);
+		let eloPoints3: number = calculateEloPoints(1, vicWith3, progressFactor);
+
+		let player1NewRank: number = player1Rank + eloPoints2 + eloPoints3;
+		let player2NewRank: number = player2Rank - eloPoints2;
+		let player3NewRank: number = player3Rank - eloPoints3;
+
+		return { player1NewRank, player2NewRank, player3NewRank };
+	}
+	else if (winner == 2) {
+		let vicWith1: number = calculateEloProbability(player2Rank, player1Rank);
+		let vicWith3: number = calculateEloProbability(player2Rank, player3Rank);
+
+		let eloPoints1: number = calculateEloPoints(1, vicWith1, progressFactor);
+		let eloPoints3: number = calculateEloPoints(1, vicWith3, progressFactor);
+
+		let player2NewRank: number = player2Rank + eloPoints1 + eloPoints3;
+		let player1NewRank: number = player1Rank - eloPoints1;
+		let player3NewRank: number = player3Rank - eloPoints3;
+
+		return { player1NewRank, player2NewRank, player3NewRank };
+	}
+	else if (winner == 3) {
+		let vicWith1: number = calculateEloProbability(player3Rank, player1Rank);
+		let vicWith2: number = calculateEloProbability(player3Rank, player2Rank);
+
+		let eloPoints1: number = calculateEloPoints(1, vicWith1, progressFactor);
+		let eloPoints2: number = calculateEloPoints(1, vicWith2, progressFactor);
+
+		let player3NewRank: number = player3Rank + eloPoints1 + eloPoints2;
+		let player1NewRank: number = player1Rank - eloPoints1;
+		let player2NewRank: number = player2Rank - eloPoints2;
+
+		return { player1NewRank, player2NewRank, player3NewRank };
+	}
+	else {
+		throw new Error("Invalid winner specified");
+	}
 }
